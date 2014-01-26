8ee4c3a2273bceb1f977c87a69b151d8867d45f5
Johan Viktorsson <johan.viktorsson.dev@gmail.com>
2014-01-26 16:30:26 +0100

    Fixed so that default projects deploy android correctly

--- build/android/create_lib_proj.sh
+++ build/android/create_lib_proj.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # Copy template android ndk files into android folder
-if [ ! -d "out/android/jni" ]; then
+if [ ! -d "out/android" ]; then
+	mkdir -p ./out
+	cp -r "${BUILD_ROOT}"/android/template_lib_proj/ out/android
+elif [ ! -d "out/android/jni" ]; then
 	cp -r "${BUILD_ROOT}"/android/template_lib_proj/* out/android/
 fi
 
